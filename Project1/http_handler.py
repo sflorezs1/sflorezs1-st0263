352528c138442dcb7843431fae0b0b5557cfb2ff
sflorezs1 <sflorezs1@eafit.edu.co>
2022-03-06 20:47:41 -0500

    Initial approach, project is not functional yet

--- Project1/http_handler.py
+++ Project1/http_handler.py
@@ -4,43 +4,50 @@ from http import HTTPStatus
 from unittest import result
 from urllib.parse import urlparse
 import json
+import socketserver
 
 from .api import API
 
 
-class HTTPMethods(Enum):
+class HTTPMethod(Enum):
     GET = 1
     POST = 2
     PUT = 3
 
-
-
 class HTTPHandler(BaseHTTPRequestHandler):
 
-    def __process_request(self, method):
+    def __init__(self, request: bytes, client_address: tuple[str, int], server: socketserver.BaseServer, api: API) -> None:
+        super().__init__(request, client_address, server)
+        self.__api = api
 
-        api = API(self)
-
+    def __process_request(self, method: HTTPMethod) -> None:
         parsed_path = urlparse(self.path)
 
         match method:
-            case HTTPMethods.GET:
-                result, status_code = api.process_request(parsed_path)()
-            case (HTTPMethods.POST | HTTPMethods.PUT):
+            case HTTPMethod.GET:
+                result, status_code = self.__api.process_request(parsed_path)()
+            case (HTTPMethod.POST | HTTPMethod.PUT):
                 content_length = int(self.headers.get('content-length', 0))
                 content = json.loads(self.rfile.read(content_length))
-                result, status_code = api.process_request(parsed_path)(**{
+                result, status_code = self.__api.process_request(parsed_path)(**{
                     'data': content
                 })
             case _:
-                raise Exception(f'Method "{method}" is not a valid method or is not allowed!')
+                result, status_code = {
+                    'message': 'Error, invalid method!',
+                }, HTTPStatus.BAD_REQUEST
+        
+        self.send_response(status_code)
+        self.send_header('Content-Type', 'application/json')
+        self.end_headers()
+        self.wfile.write(result)
         
 
-    def do_GET(self):
-        return self.__process_request(HTTPMethods.GET)
+    def do_GET(self) -> None:
+        self.__process_request(HTTPMethod.GET)
 
-    def do_POST(self):
-        return self.__process_request(HTTPMethods.POST)
+    def do_POST(self) -> None:
+        self.__process_request(HTTPMethod.POST)
 
-    def do_PUT(self):
-        return self.__process_request(HTTPMethods.PUT)
+    def do_PUT(self) -> None:
+        self.__process_request(HTTPMethod.PUT)
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # sflorezs1-st0263
+
+Repository for homework submission for ST0263 - Special Topics on Telematics course on Universidad EAFIT - Colombia.
+
+## Author
+
+Simón Flórez Silva (sflorezs1@eafit.edu.co)
+
+a
